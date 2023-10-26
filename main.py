6915f235327657841b8128ef0a871e9d444735e9
Alexander <aleks.aleksorion@gmail.com>
2023-10-26 20:42:46 +0300

    from 1 to 10 prog

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print1_10(i):
+    while i < 11:
+        print(i)
+        i += 1
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+i = 0
+print1_10(i)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
